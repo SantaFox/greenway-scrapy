108810d15d7d007ef18b4e5c5ca4330f73676de9
santafox <santafox@gmail.com>
2021-02-22 20:27:30 +0200

    Update Spider to work on pages

--- tutorial/tutorial/spiders/itemsSpider.py
+++ tutorial/tutorial/spiders/itemsSpider.py
@@ -9,12 +9,13 @@ class ItemsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes={page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        items_links = response.css('div.catalog-item a.catalog-item-title::attr(href)')
+        yield from response.follow_all(items_links, self.parse_item)
 
-'''item = response.css('div.catalog-item')[0]
-item.css('div.catalog-item-code::text').get()
-item.css('a.catalog-item-title::attr(href)').get()
-'''
+    def parse_item(self, response):
+        product_page = response.css('section.product-page')
+        yield {
+            'name': product_page.css('section.page-head h1::text').get(),
+            'code': product_page.css('div.product-main-info h4::text').get(),
+            'pics': product_page.css('div.gallery-thumb img::attr(data-zoom-image)').getall(),
+        }
